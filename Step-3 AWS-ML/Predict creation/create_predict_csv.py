cf20f4c255a482aea644c20b7bc6a001c8312dad
ArnaudT13 <55779980+ArnaudT13@users.noreply.github.com>
2021-02-05 18:45:17 +0100

    Adding predict csv file script creation

---
+++ Step-3 AWS-ML/Predict creation/create_predict_csv.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+print('[INFO] Loading data')
+data = pd.read_json("../Notebook/data.json")
+
+print('[INFO] Splitting predict dataset')
+train, predict = train_test_split(data, test_size=0.2, random_state = 211101)
+
+# Splitting predictions that will be exported
+print('[INFO] Create predict csv file')
+df_predict = pd.DataFrame(predict)
+df_predict.to_csv('Result.csv')
