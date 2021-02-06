9e761a046dcb2e4a9687c096f9b3a737025385fe
ArnaudT13 <55779980+ArnaudT13@users.noreply.github.com>
2021-02-06 15:19:07 +0100

    Adding constants in create predict script

--- Step-3 AWS-ML/Predict creation/create_predict_csv.py
+++ Step-3 AWS-ML/Predict creation/create_predict_csv.py
@@ -3,18 +3,24 @@ Description:
     Split the data.json file to obtain the predict part
 '''
 
+## Constants
+DATA_JSON_PATH = "../Data/data.json"
+PREDICT_FILE = "Predict.csv"
+
+
+## Imports
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-data_json_path = "../Notebook/data.json"
 
+## Load and split dataset
 print('[INFO] Loading data')
-data = pd.read_json(data_json_path)
+data = pd.read_json(DATA_JSON_PATH)
 
 print('[INFO] Splitting predict dataset')
 train, predict = train_test_split(data, test_size=0.2, random_state = 211101)
 
-# Splitting predictions that will be exported
+## Export prediction
 print('[INFO] Create predict csv file')
 df_predict = pd.DataFrame(predict)
-df_predict.to_csv('Predict.csv')
+df_predict.to_csv(PREDICT_FILE)
