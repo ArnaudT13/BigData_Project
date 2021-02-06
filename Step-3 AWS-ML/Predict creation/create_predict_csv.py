767af7c5f49e7d0d26e53b99579a4ae00d4107d6
ArnaudT13 <55779980+ArnaudT13@users.noreply.github.com>
2021-02-06 11:56:56 +0100

    Add description to create predict csv

--- Step-3 AWS-ML/Predict creation/create_predict_csv.py
+++ Step-3 AWS-ML/Predict creation/create_predict_csv.py
@@ -1,8 +1,15 @@
+'''
+Description:
+    Split the data.json file to obtain the predict part
+'''
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+data_json_path = "../Notebook/data.json"
+
 print('[INFO] Loading data')
-data = pd.read_json("../Notebook/data.json")
+data = pd.read_json(data_json_path)
 
 print('[INFO] Splitting predict dataset')
 train, predict = train_test_split(data, test_size=0.2, random_state = 211101)
