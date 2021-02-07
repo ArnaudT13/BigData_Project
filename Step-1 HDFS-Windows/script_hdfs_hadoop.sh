c39d74d9caf330726af2d0a9611dd302aec5849b
Cédric Gormond <cedric.gormond@gmail.com>
2021-02-07 20:57:20 +0100

    Add script hdfs to hadoop and rename hadoop windows file

---
+++ Step-1 HDFS-Windows/script_hdfs_hadoop.sh
@@ -0,0 +1,47 @@
+PROJECTS 
+
+[
+    {
+        "id": 6,
+        "name": "Kanban",
+        "company": "TSE",
+        "creationDate": "2020-11-01",
+        "times": []
+    },
+    {
+        "id": 7,
+        "name": "POC",
+        "company": "TSE",
+        "creationDate": "2020-12-01",
+        "times": [
+            {
+                "id": 8,
+                "duration": 0.2,
+                "description": "Front End development",
+                "creationDate": "2020-12-14",
+                "processDate": "2021-01-29",
+                "user": null,
+                "project": {
+                    "id": 7,
+                    "name": "POC",
+                    "company": "TSE",
+                    "creationDate": "2020-12-01"
+                }
+            },
+            {
+                "id": 9,
+                "duration": 0.5,
+                "description": "Back End development",
+                "creationDate": "2020-12-15",
+                "processDate": "2021-01-29",
+                "user": null,
+                "project": {
+                    "id": 7,
+                    "name": "POC",
+                    "company": "TSE",
+                    "creationDate": "2020-12-01"
+                }
+            }
+        ]
+    }
+]
